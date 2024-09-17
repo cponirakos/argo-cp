8e359471310d51809ecdb3f28264ea3ccbbd609e
cponirakos <c.ponirakos@deepsea.ai>
2024-09-17 14:50:48 +0300

    cluster size to medium to test gatekeeper policy

--- env-setup-scripts/argoCD.sh
+++ env-setup-scripts/argoCD.sh
@@ -15,7 +15,7 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 echo "Hang tight, waiting for argoCD server to become ready. When you see the port-forward console open, you can access the argoCD UI at https://localhost:8080 with username admin and the password you provided."
 until kubectl get pods -l app.kubernetes.io/name=argocd-server -n argocd -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}' | grep -m 1 "True"; do : ; done
-osascript -e 'tell app "Terminal" to do script "kubectl port-forward svc/argocd-server -n argocd 8080:443"'
+#osascript -e 'tell app "Terminal" to do script "kubectl port-forward svc/argocd-server -n argocd 8080:443"'
 export ARGO_ADMIN_PW=$1
 export ARGO_NEW_PW=$(htpasswd -bnB "" $ARGO_ADMIN_PW | tr -d ':\n' | sed 's/$2y/$2a/')
 kubectl -n argocd patch secret argocd-secret   -p '{"stringData": {"admin.password": "'$ARGO_NEW_PW'","admin.passwordMtime": "'$(date +%FT%T%Z)'"}}'
